d0b35bd28463b0f1875fd26f35b55bc33cef634e
lorenzowind <lorenzomart01@gmail.com>
2020-07-04 23:03:24 -0400

    feat: dashboard styling

--- Level03/project-github-explorer/src/pages/Dashboard/index.tsx
+++ Level03/project-github-explorer/src/pages/Dashboard/index.tsx
@@ -1,9 +1,98 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="button">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48891746?s=460&u=c53ef00e81830ae2549ded78efff6a68c2c97f10&v=4"
+            alt="Lorenzo Windmoller"
+          />
+          <div>
+            <strong>Code Helpers</strong>
+            <p>
+              The Code Helpers is an application to share errors or programming
+              difficulties
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48891746?s=460&u=c53ef00e81830ae2549ded78efff6a68c2c97f10&v=4"
+            alt="Lorenzo Windmoller"
+          />
+          <div>
+            <strong>Code Helpers</strong>
+            <p>
+              The Code Helpers is an application to share errors or programming
+              difficulties
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48891746?s=460&u=c53ef00e81830ae2549ded78efff6a68c2c97f10&v=4"
+            alt="Lorenzo Windmoller"
+          />
+          <div>
+            <strong>Code Helpers</strong>
+            <p>
+              The Code Helpers is an application to share errors or programming
+              difficulties
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48891746?s=460&u=c53ef00e81830ae2549ded78efff6a68c2c97f10&v=4"
+            alt="Lorenzo Windmoller"
+          />
+          <div>
+            <strong>Code Helpers</strong>
+            <p>
+              The Code Helpers is an application to share errors or programming
+              difficulties
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/48891746?s=460&u=c53ef00e81830ae2549ded78efff6a68c2c97f10&v=4"
+            alt="Lorenzo Windmoller"
+          />
+          <div>
+            <strong>Code Helpers</strong>
+            <p>
+              The Code Helpers is an application to share errors or programming
+              difficulties
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
