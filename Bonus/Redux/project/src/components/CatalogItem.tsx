a1d3dfca0d8139a2a0891b17b28388035ca6a97e
lorenzowind <lorenzomart01@gmail.com>
2021-01-15 17:33:11 -0400

    feat: stock validation

--- Bonus/Redux/project/src/components/CatalogItem.tsx
+++ Bonus/Redux/project/src/components/CatalogItem.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { IProduct } from '../store/modules/cart/types';
 
-import { addProductToCart } from '../store/modules/cart/action';
+import { addProductToCartRequest } from '../store/modules/cart/action';
+import { IState } from '../store';
 
 interface CatalogItemProps {
   product: IProduct;
@@ -12,8 +13,12 @@ interface CatalogItemProps {
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
 
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id);
+  });
+
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
 
   return (
@@ -28,6 +33,12 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
       <strong>{` - ${product.title} `}</strong>
       
       <span>{product.price}</span>
+
+      { hasFailedStockCheck && 
+        <span style={{ color: 'red' }}>
+          Falta de estoque
+        </span> 
+      }
     </article>
   )
 };
